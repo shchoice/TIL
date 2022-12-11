42e7ee942fc2f75c551f36bbfb16e879871b6d47
Seohwan Choi <shchoice7140@gmail.com>
2022-12-12 01:47:48 +0900

    Spring IoC 컨테이너 - Autowire 빈이 여러개인 경우 with Qualifier

--- Spring/Spring-Autowire/src/main/java/my/study/springautowire/BookService.java
+++ Spring/Spring-Autowire/src/main/java/my/study/springautowire/BookService.java
@@ -1,11 +1,13 @@
 package my.study.springautowire;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BookService {
   @Autowired
+  @Qualifier("myBookRepository")
   BookRepository bookRepository;
 
   public void printBookRepository() {
--- Spring/Spring-Autowire/src/main/java/my/study/springautowire/MyBookRepository.java
+++ Spring/Spring-Autowire/src/main/java/my/study/springautowire/MyBookRepository.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Repository;
 
 @Repository
-@Primary
 public class MyBookRepository implements BookRepository {
 
 }
