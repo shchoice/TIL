16d223af37434cba9e7c6e25c02a387a37df3b2c
Seohwan Choi <shchoice7140@gmail.com>
2022-12-15 00:31:28 +0900

    Spring IoC 컨테이너 - Environment (1), Profile - getProfiles

--- Spring/Spring-Autowire/src/main/java/my/study/bean/scope/AppRunner.java
+++ Spring/Spring-Autowire/src/main/java/my/study/bean/scope/AppRunner.java
@@ -1,9 +1,11 @@
 package my.study.bean.scope;
 
+import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.ApplicationContext;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -14,33 +16,9 @@ public class AppRunner implements ApplicationRunner {
 
   @Override
   public void run(ApplicationArguments args) throws Exception {
-    System.out.println("Proto");
-    System.out.println(applicationContext.getBean(Proto.class));
-    System.out.println(applicationContext.getBean(Proto.class));
-    System.out.println(applicationContext.getBean(Proto.class));
+    Environment environment = applicationContext.getEnvironment();
+    System.out.println(Arrays.toString(environment.getActiveProfiles())); // []
+    System.out.println(Arrays.toString(environment.getDefaultProfiles())); // [default]
 
-    System.out.println("Singleton");
-    System.out.println(applicationContext.getBean(Single.class));
-    System.out.println(applicationContext.getBean(Single.class));
-    System.out.println(applicationContext.getBean(Single.class));
-
-    System.out.println("Proto by Single");
-    System.out.println(applicationContext.getBean(Single.class).getProto());
-    System.out.println(applicationContext.getBean(Single.class).getProto());
-    System.out.println(applicationContext.getBean(Single.class).getProto());
-    /*
-    Proto
-    my.study.bean.scope.Proto@7102ac3e
-    my.study.bean.scope.Proto@3f598450
-    my.study.bean.scope.Proto@2db2a05f
-    Singleton
-    my.study.bean.scope.Single@695c938d
-    my.study.bean.scope.Single@695c938d
-    my.study.bean.scope.Single@695c938d
-    Proto by Single
-    my.study.bean.scope.Proto@d5bb1c4
-    my.study.bean.scope.Proto@7ed3df3b
-    my.study.bean.scope.Proto@465b38e6
-     */
   }
 }
--- Spring/Spring-Autowire/src/main/java/my/study/bean/scope/Single.java
+++ Spring/Spring-Autowire/src/main/java/my/study/bean/scope/Single.java
@@ -2,6 +2,7 @@ package my.study.bean.scope;
 
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -10,6 +11,9 @@ public class Single {
   // Single 빈이 싱글톤 빈이기 때문에 Proto 타입의 proto 빈을 받아도 변하지 않음 -> proto 빈은 변해야하는데 의도와 다름
   @Autowired
   private ObjectProvider<Proto> proto;
+
+  @Autowired
+  ApplicationContext applicationContext;
 
   public Proto getProto() {
     return proto.getIfAvailable();
