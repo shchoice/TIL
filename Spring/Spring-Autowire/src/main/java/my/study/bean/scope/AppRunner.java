c8c84ef79f0a4c77ca846c33a752580174961565
Seohwan Choi <shchoice7140@gmail.com>
2022-12-14 11:05:54 +0900

    Spring IoC 컨테이너 - 빈의 스코프, Proto (2)

--- Spring/Spring-Autowire/src/main/java/my/study/bean/scope/AppRunner.java
+++ Spring/Spring-Autowire/src/main/java/my/study/bean/scope/AppRunner.java
@@ -3,21 +3,25 @@ package my.study.bean.scope;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AppRunner implements ApplicationRunner {
 
   @Autowired
-  Single single;
-
-  @Autowired
-  Proto proto;
+  ApplicationContext applicationContext;
 
   @Override
   public void run(ApplicationArguments args) throws Exception {
-    // 두 인스턴스가 같음을 알 수 있다.
-    System.out.println(proto);
-    System.out.println(single.getProto());
+    System.out.println("Proto");
+    System.out.println(applicationContext.getBean(Proto.class));
+    System.out.println(applicationContext.getBean(Proto.class));
+    System.out.println(applicationContext.getBean(Proto.class));
+
+    System.out.println("Singleton");
+    System.out.println(applicationContext.getBean(Single.class));
+    System.out.println(applicationContext.getBean(Single.class));
+    System.out.println(applicationContext.getBean(Single.class));
   }
 }
