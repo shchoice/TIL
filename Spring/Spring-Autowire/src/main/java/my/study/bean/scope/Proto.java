d36a0b29d6979af73c19e547374b1c79d77461fd
Seohwan Choi <shchoice7140@gmail.com>
2022-12-14 11:02:55 +0900

    Spring IoC 컨테이너 - 빈의 스코프, Proto (1)

--- Spring/Spring-Autowire/src/main/java/my/study/bean/scope/Proto.java
+++ Spring/Spring-Autowire/src/main/java/my/study/bean/scope/Proto.java
@@ -1,8 +1,10 @@
 package my.study.bean.scope;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope("prototype")
 public class Proto {
 
 }
