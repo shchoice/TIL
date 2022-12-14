4b0d50abbce5e50d591d3de10d9b974bda2a529a
Seohwan Choi <shchoice7140@gmail.com>
2022-12-15 00:49:35 +0900

    Spring IoC 컨테이너 - Environment (1), Profile - 빈에 프로파일 적용 + 프로파일 표현식

--- Spring/Spring-Autowire/src/main/java/my/study/bean/scope/TestBookRepository.java
+++ Spring/Spring-Autowire/src/main/java/my/study/bean/scope/TestBookRepository.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Repository;
 
 @Repository
-@Scope("test")
+@Scope("!prod & test")
 public class TestBookRepository implements BookRepository {
 
 }
