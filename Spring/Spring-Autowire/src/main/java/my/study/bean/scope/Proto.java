b3f7ef3b0baf0877e76cea05d92fc8eab260c172
Seohwan Choi <shchoice7140@gmail.com>
2022-12-14 23:55:30 +0900

    Spring IoC 컨테이너 - 빈의 스코프, Proto (3) 싱글톤 빈이 프로토타입 빈을 참조(ObjectProvider)

--- Spring/Spring-Autowire/src/main/java/my/study/bean/scope/Proto.java
+++ Spring/Spring-Autowire/src/main/java/my/study/bean/scope/Proto.java
@@ -6,10 +6,7 @@ import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 
 @Component
-@Scope(value = "prototype", proxyMode = ScopedProxyMode.TARGET_CLASS)
-// 클래스 기반의 프록시로 감싸라. Single Singleton이 직접 참조하지 않고 Proxy를 거쳐 참조해야함
-// 매번 바꿔줄 수 있도록 프록시로 감싼 것임!
-// CG 라이브러리로 클래스도 프록시로 만들게 해줌(Java 프록시는 인터페이스만 가능한 반면)
+@Scope(value = "prototype")
 public class Proto {
 
   // Proto Type 빈에서 Singleton 빈을 참조하는 것은 전혀 문제가 없음
--- Spring/Spring-Autowire/src/main/java/my/study/bean/scope/Single.java
+++ Spring/Spring-Autowire/src/main/java/my/study/bean/scope/Single.java
@@ -1,5 +1,6 @@
 package my.study.bean.scope;
 
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -8,9 +9,9 @@ public class Single {
 
   // Single 빈이 싱글톤 빈이기 때문에 Proto 타입의 proto 빈을 받아도 변하지 않음 -> proto 빈은 변해야하는데 의도와 다름
   @Autowired
-  private Proto proto;
+  private ObjectProvider<Proto> proto;
 
   public Proto getProto() {
-    return proto;
+    return proto.getIfAvailable();
   }
 }
