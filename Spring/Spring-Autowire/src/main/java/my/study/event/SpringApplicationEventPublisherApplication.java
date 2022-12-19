866844c034b75d7dd04f000ccff9be44a0769a0c
Seohwan Choi <shchoice7140@gmail.com>
2022-12-19 23:50:58 +0900

    Spring IoC 컨테이너 - ApplicationEventPublisher (2) - 복수의 eventhandler 있을 시 - 순서 보장x - Async

--- Spring/Spring-Autowire/src/main/java/my/study/event/SpringApplicationEventPublisherApplication.java
+++ Spring/Spring-Autowire/src/main/java/my/study/event/SpringApplicationEventPublisherApplication.java
@@ -4,15 +4,17 @@ import my.study.springautowire.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class SpringApplicationEventPublisherApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(SpringApplicationEventPublisherApplication.class, args);
 //    Thread[main,5,main]
-//    Event Received, Data : 100
-//    Thread[main,5,main]
 //    Another 100
+//    Thread[task-1,5,main]
+//    Event Received, Data : 100
   }
 }
