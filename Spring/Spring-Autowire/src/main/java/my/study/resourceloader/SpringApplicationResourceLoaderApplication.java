ca828f9d545b3eed69bc3c5a18ca5115e6f9c68c
Seohwan Choi <shchoice7140@gmail.com>
2022-12-20 09:24:47 +0900

    Spring IoC 컨테이너- ResourceLoader

---
+++ Spring/Spring-Autowire/src/main/java/my/study/resourceloader/SpringApplicationResourceLoaderApplication.java
@@ -0,0 +1,12 @@
+package my.study.resourceloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApplicationResourceLoaderApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringApplicationResourceLoaderApplication.class, args);
+  }
+}
