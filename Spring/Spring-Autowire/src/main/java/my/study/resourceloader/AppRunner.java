f2a5450e3f011b75db2dc600766de830f3c762d6
Seohwan Choi <shchoice7140@gmail.com>
2022-12-20 09:28:00 +0900

    Spring IoC 컨테이너- ResourceLoader

--- Spring/Spring-Autowire/src/main/java/my/study/resourceloader/AppRunner.java
+++ Spring/Spring-Autowire/src/main/java/my/study/resourceloader/AppRunner.java
@@ -1,5 +1,8 @@
 package my.study.resourceloader;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -16,6 +19,8 @@ public class AppRunner implements ApplicationRunner {
   @Override
   public void run(ApplicationArguments args) throws Exception {
     Resource resource = resourceLoader.getResource("classpath:test.txt");
-    System.out.println(resource.exists());
+    System.out.println(resource.exists()); // true
+    System.out.println(resource.getDescription()); // class path resource [test.txt]
+    System.out.println(Files.readString(Path.of(resource.getURI()))); // Hello Spring!
   }
 }
