75143e398bdf8a3712bd1eddf0ed535e846badb7
Seohwan Choi <shchoice7140@gmail.com>
2022-12-26 09:56:23 +0900

    Validation (1)

---
+++ Spring/Spring-Autowire/src/main/java/my/study/validation/AppRunner.java
@@ -0,0 +1,34 @@
+package my.study.validation;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.BeanPropertyBindingResult;
+import org.springframework.validation.Errors;
+
+@Component
+public class AppRunner implements ApplicationRunner {
+
+  @Override
+  public void run(ApplicationArguments args) throws Exception {
+    Event event = new Event();
+    EventValidator eventValidator = new EventValidator();
+    Errors errors = new BeanPropertyBindingResult(event, "event");
+
+    eventValidator.validate(event, errors);
+
+    System.out.println(errors.hasErrors());
+
+    errors.getAllErrors().forEach(e -> {
+      System.out.println("===== Error Code ====");
+      Arrays.stream(e.getCodes()).forEach(System.out::println);
+      System.out.println(e.getDefaultMessage());
+    });
+  }
+}
---
+++ Spring/Spring-Autowire/src/main/java/my/study/validation/SpringApplicationValidationApplication.java
@@ -0,0 +1,12 @@
+package my.study.validation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApplicationValidationApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringApplicationValidationApplication.class, args);
+  }
+}
