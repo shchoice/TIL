c2f5f8f1eb02ccd5fa1b48f969df6c042f76d913
Seohwan Choi <shchoice7140@gmail.com>
2022-12-26 10:07:49 +0900

    Validation (1) - ValidationUtils가 아닌 직접 에러명 만들기

--- Spring/Spring-Autowire/src/main/java/my/study/validation/EventValidator.java
+++ Spring/Spring-Autowire/src/main/java/my/study/validation/EventValidator.java
@@ -14,5 +14,10 @@ public class EventValidator implements Validator {
   @Override
   public void validate(Object target, Errors errors) {
     ValidationUtils.rejectIfEmptyOrWhitespace(errors, "title", "notempty", "Empty title is not allowed");
+    //ValidationUtils 객체 말고 직접 코딩하려면
+    Event event = (Event) target;
+    if (event.getTitle() == null) {
+      errors.reject("Empty Title");
+    }
   }
 }
