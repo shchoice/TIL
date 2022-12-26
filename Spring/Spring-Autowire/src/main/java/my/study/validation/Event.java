3cb69d7cb03c47ccebd1b1973c00e9a2228f3b99
Seohwan Choi <shchoice7140@gmail.com>
2022-12-27 00:18:26 +0900

    Validation (2) validation.constraints 어노테이션으로 검증

--- Spring/Spring-Autowire/src/main/java/my/study/validation/Event.java
+++ Spring/Spring-Autowire/src/main/java/my/study/validation/Event.java
@@ -1,8 +1,24 @@
 package my.study.validation;
 
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class Event {
   private int id;
-  String title;
+  @NotEmpty
+  private String title;
+
+  @NotNull
+  @Min(0) @Max(10)
+  private Integer limit;
+
+  @Email
+  String email;
 
   public int getId() {
     return id;
@@ -18,5 +34,21 @@ public class Event {
 
   public void setTitle(String title) {
     this.title = title;
+  }
+
+  public Integer getLimit() {
+    return limit;
+  }
+
+  public void setLimit(Integer limit) {
+    this.limit = limit;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
   }
 }
