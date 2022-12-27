479d04a0404bd237320ff7924515f21cfe82cc57
Seohwan Choi <shchoice7140@gmail.com>
2022-12-28 00:45:21 +0900

    DataBinding (2) - Converter

---
+++ Spring/Spring-Autowire/src/main/java/my/study/databinding2/SpringApplicationDataBinding2Application.java
@@ -0,0 +1,12 @@
+package my.study.databinding2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApplicationDataBinding2Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringApplicationDataBinding2Application.class, args);
+  }
+}
