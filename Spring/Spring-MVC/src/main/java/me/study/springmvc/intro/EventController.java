31dbdf0ba986ea3f62e07ce17f0f3bbba90fe23b
Seohwan Choi <shchoice7140@gmail.com>
2023-01-02 23:47:42 +0900

    Spring MVC Introduction

---
+++ Spring/Spring-MVC/src/main/java/me/study/springmvc/intro/EventController.java
@@ -0,0 +1,22 @@
+package me.study.springmvc.intro;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class EventController {
+
+  @Autowired
+  EventService eventService;
+
+  // 예전 방식 - @RequestMapping(value = "/events", method = RequestMethod.GET)
+  @GetMapping("/events")
+  public String events(Model model) {
+    model.addAttribute("events", eventService.getEvents());
+    return "events";
+  }
+
+}
---
+++ Spring/Spring-MVC/src/main/java/me/study/springmvc/intro/EventService.java
@@ -0,0 +1,26 @@
+package me.study.springmvc.intro;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EventService {
+  public List<Event> getEvents() {
+    Event event01 = Event.builder()
+        .name("Spring Web MVC Study - (1)")
+        .limitOfEnrollment(5)
+        .startDateTime(LocalDateTime.of(2022, 12, 30, 18,0))
+        .endDateTime(LocalDateTime.of(2023, 1, 7, 23, 50))
+        .build();
+
+    Event event02 = Event.builder()
+        .name("Spring Web MVC Study - (2)")
+        .limitOfEnrollment(5)
+        .startDateTime(LocalDateTime.of(2023, 1, 8, 0,0))
+        .endDateTime(LocalDateTime.of(2023, 1, 15, 23, 50))
+        .build();
+
+    return List.of(event01, event02);
+  }
+}
