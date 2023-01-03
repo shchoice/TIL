f225b6b95d0d621ad1d12ba24732cd9215793892
Seohwan Choi <shchoice7140@gmail.com>
2023-01-04 00:55:25 +0900

    Spring MVC - Servlet 이란

--- Spring/Spring-Servlet/src/main/java/me/study/springmvc/servlet/HelloServlet.java
+++ Spring/Spring-Servlet/src/main/java/me/study/springmvc/servlet/HelloServlet.java
@@ -14,11 +14,7 @@ public class HelloServlet extends HttpServlet {
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
     System.out.println("doGet");
-    resp.getWriter().write("Hello " + getName());
-  }
-
-  private Object getName() {
-    return getServletContext().getAttribute("name");
+    resp.getWriter().write("Hello Servlet");
   }
 
   @Override
