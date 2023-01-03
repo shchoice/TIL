c23f2835d063dbd5c5c4f3244240ffa44d3c9493
Seohwan Choi <shchoice7140@gmail.com>
2023-01-04 01:12:17 +0900

    Spring MVC - Servlet Filter 란

--- Spring/Spring-Servlet/src/main/java/me/study/springmvc/servlet/HelloServlet.java
+++ Spring/Spring-Servlet/src/main/java/me/study/springmvc/servlet/HelloServlet.java
@@ -17,13 +17,17 @@ public class HelloServlet extends HttpServlet {
     resp.getWriter().println("<h1>Hello " + getName() + "</h1>");
     // 1. 톰캣 기동 시
     // Context Initialized
+    // Filter Init
     // 2. 첫번째 요청 시(http://localhost:8080/hello)
     // init
+    // Filter
     // doGet
     // 3.두번째 요청 시
+    // Filter
     // doGet
     // 4. 톰캣 서버 종료 시
     // destory
+    // Filter Destory
     // Context Destroyed
   }
 
