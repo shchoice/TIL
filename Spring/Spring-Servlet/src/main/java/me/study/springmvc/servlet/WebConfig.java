c6db43378be5b8f9eb0abfcc42bb7eb9de5a7646
Seohwan Choi <shchoice7140@gmail.com>
2023-01-09 12:27:07 +0900

    Spring MVC - Servelt + Spring IoC + MVC (Servlet WebApplicationContext 만 사용)

--- Spring/Spring-Servlet/src/main/java/me/study/springmvc/servlet/WebConfig.java
+++ Spring/Spring-Servlet/src/main/java/me/study/springmvc/servlet/WebConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 @Configuration
-@ComponentScan(useDefaultFilters = false, includeFilters = @ComponentScan.Filter(Component.class))
+@ComponentScan
 public class WebConfig {
 
 }
