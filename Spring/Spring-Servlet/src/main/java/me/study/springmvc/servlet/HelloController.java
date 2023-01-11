1506fce341d014e68798534810490d711f9588f9
Seohwan Choi <shchoice7140@gmail.com>
2023-01-11 23:05:57 +0900

    Spring MVC - Servelt + Spring IoC + MVC (View가 있을 경우)

--- Spring/Spring-Servlet/src/main/java/me/study/springmvc/servlet/HelloController.java
+++ Spring/Spring-Servlet/src/main/java/me/study/springmvc/servlet/HelloController.java
@@ -1,7 +1,9 @@
 package me.study.springmvc.servlet;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /*
@@ -9,14 +11,22 @@ import org.springframework.web.bind.annotation.RestController;
  * 어떤 요청을 처리하는 Handler를 GetMapping으로 처리하도록 만들어야함.
  * -> 즉 핸들러 쪽으로 요청을 dispatch 해주고, 애노테인션을 이해하고, 응답로 HTTP return을 HTTP Response로 리턴해줄 수 있는 DispatcherServlet을 써야함!
  */
-@RestController
+@Controller
+//@RestController : @ResponseBody를 사용한 Controlller 와 동일
 public class HelloController {
 
   @Autowired
   HelloService helloService;
 
   @GetMapping("/hello")
+  @ResponseBody
   public String hello() {
     return "Hello, " + helloService.getName();
   }
+
+  @GetMapping("/sample")
+  public String sample() {
+    return "/WEB-INF/sample.jsp";
+  }
+
 }
