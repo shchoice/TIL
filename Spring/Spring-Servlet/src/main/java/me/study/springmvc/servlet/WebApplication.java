e7cd93f81f5180813a2061f598c6305ad9de2dd1
Seohwan Choi <shchoice7140@gmail.com>
2023-01-14 23:20:03 +0900

    Spring MVC - WebApplicationInitializer(web.xml 없이 사용하기)

---
+++ Spring/Spring-Servlet/src/main/java/me/study/springmvc/servlet/WebApplication.java
@@ -0,0 +1,23 @@
+package me.study.springmvc.servlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class WebApplication implements WebApplicationInitializer {
+
+  @Override
+  public void onStartup(ServletContext servletContext) throws ServletException {
+    AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+    context.setServletContext(servletContext);
+    context.register(WebConfig.class);
+    context.refresh();
+
+    DispatcherServlet dispatcherServlet = new DispatcherServlet(context);
+    ServletRegistration.Dynamic app = servletContext.addServlet("app", dispatcherServlet);
+    app.addMapping("/app/*");
+  }
+}
