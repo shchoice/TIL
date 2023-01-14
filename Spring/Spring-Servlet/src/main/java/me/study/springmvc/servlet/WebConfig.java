6db2b8f359cf1b58cbf5b2952f3b6e953f8c9c54
Seohwan Choi <shchoice7140@gmail.com>
2023-01-15 01:00:38 +0900

    Spring MVC - 스프링 MVC 빈 설정

--- Spring/Spring-Servlet/src/main/java/me/study/springmvc/servlet/WebConfig.java
+++ Spring/Spring-Servlet/src/main/java/me/study/springmvc/servlet/WebConfig.java
@@ -3,13 +3,33 @@ package me.study.springmvc.servlet;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerAdapter;
+import org.springframework.web.servlet.HandlerMapping;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @ComponentScan
 public class WebConfig {
+
+  @Bean
+  public HandlerMapping handlerMapping() {
+    RequestMappingHandlerMapping handlerMapping = new RequestMappingHandlerMapping();
+    handlerMapping.setOrder(Ordered.HIGHEST_PRECEDENCE);
+
+    return handlerMapping;
+  }
+
+  @Bean
+  public HandlerAdapter handlerAdapter() {
+    RequestMappingHandlerAdapter handlerAdapter = new RequestMappingHandlerAdapter();
+
+    return handlerAdapter;
+  }
 
   @Bean
   public ViewResolver viewResolver() {
