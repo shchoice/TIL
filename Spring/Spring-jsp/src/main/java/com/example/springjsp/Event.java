958de0fef250271ba668785fd80fbf0319f16aea
Seohwan Choi <shchoice7140@gmail.com>
2023-01-15 23:50:59 +0900

    Spring MVC - 스프링부트와 JSP(권장x)

---
+++ Spring/Spring-jsp/src/main/java/com/example/springjsp/Event.java
@@ -0,0 +1,24 @@
+package com.example.springjsp;
+
+import java.time.LocalDateTime;
+
+public class Event {
+  private String name;
+  private LocalDateTime starts;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public LocalDateTime getStarts() {
+    return starts;
+  }
+
+  public void setStarts(LocalDateTime starts) {
+    this.starts = starts;
+  }
+}
