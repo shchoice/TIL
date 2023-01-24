f8afd86f62313314a12b27b34b3e2920893f9515
Seohwan Choi <shchoice7140@gmail.com>
2023-01-24 19:28:03 +0900

    Spring Intro - Basic(API)

--- Spring/Lecture/SpringIntroduction/src/main/java/my/stduy/springintroduction/controller/HelloController.java
+++ Spring/Lecture/SpringIntroduction/src/main/java/my/stduy/springintroduction/controller/HelloController.java
@@ -1,9 +1,12 @@
 package my.stduy.springintroduction.controller;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -18,5 +21,26 @@ public class HelloController {
   public String helloMvc(@RequestParam(value = "name", required = true) String name, Model model) {
     model.addAttribute("name", name);
     return "hello-template";
+  }
+
+  @GetMapping("hello-string")
+  @ResponseBody
+  public String helloString(@RequestParam("name") String name) {
+    return "hello " + name; // HTML 소스를 보면 HTML 태그 등의 HTML 템플릿이 없고 데이터를 그대로 보여줌
+  }
+
+  @GetMapping("hello-api")
+  @ResponseBody
+  public Hello helloApi(@RequestParam("name") String name) {
+    Hello hello = new Hello();
+    hello.setName(name);
+
+    return hello;
+  }
+
+  @Getter
+  @Setter
+  static class Hello {
+    private String name;
   }
 }
