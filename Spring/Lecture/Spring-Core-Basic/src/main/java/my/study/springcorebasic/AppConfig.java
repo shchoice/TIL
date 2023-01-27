36dbd944cd3816ca63fb7925f0d8b1d64cf5e0b1
Seohwan Choi <shchoice7140@gmail.com>
2023-01-27 11:34:50 +0900

    Spring Core Basic - POJO 수준의 회원/주문 도메인 예제(DIP, OCP 위반 제거) + AppConfig 리팩터링(중복 및 구현부 수정, SRP 적용)

--- Spring/Lecture/Spring-Core-Basic/src/main/java/my/study/springcorebasic/AppConfig.java
+++ Spring/Lecture/Spring-Core-Basic/src/main/java/my/study/springcorebasic/AppConfig.java
@@ -1,6 +1,9 @@
 package my.study.springcorebasic;
 
+import my.study.springcorebasic.discount.DiscountPolicy;
 import my.study.springcorebasic.discount.FixDiscountPolicy;
+import my.study.springcorebasic.discount.RateDiscountPolicy;
+import my.study.springcorebasic.member.MemberRepository;
 import my.study.springcorebasic.member.MemberService;
 import my.study.springcorebasic.member.MemberServiceImpl;
 import my.study.springcorebasic.member.MemoryMemberRepository;
@@ -9,13 +12,22 @@ import my.study.springcorebasic.order.OrderServiceImpl;
 
 public class AppConfig {
 
+  public MemberRepository memberRepository() {
+    return new MemoryMemberRepository();
+  }
+
   public MemberService memberService() {
-    return new MemberServiceImpl(new MemoryMemberRepository());
+    return new MemberServiceImpl(memberRepository());
   }
 
   public OrderService orderService() {
     return new OrderServiceImpl(
-        new MemoryMemberRepository(),
-        new FixDiscountPolicy());
+        memberRepository(),
+        discountPolicy());
+  }
+
+  public DiscountPolicy discountPolicy() {
+    // return new FixDiscountPolicy();
+    return new RateDiscountPolicy();
   }
 }
