5d4adcaa0a8765257d1645f9be1cdc809bbd4c69
Seohwan Choi <shchoice7140@gmail.com>
2023-01-28 12:31:39 +0900

    Spring Core Basic - 스프링 컨테이너와 스프링 빈(다양한 설정 형식 지원, XML)

---
+++ Spring/Lecture/Spring-Core-Basic/src/test/java/my/study/springcorebasic/xml/XmlAppContext.java
@@ -0,0 +1,19 @@
+package my.study.springcorebasic.xml;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import my.study.springcorebasic.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class XmlAppContext {
+  @Test
+  void xmlAppContext() {
+    ApplicationContext context = new GenericXmlApplicationContext("appConfig.xml");
+
+    MemberService memberService = context.getBean("memberService", MemberService.class);
+    assertThat(memberService).isInstanceOf(MemberService.class);
+  }
+}
