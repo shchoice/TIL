3e27880551ce4a82a563444558550ed8957e7d21
Seohwan Choi <shchoice7140@gmail.com>
2023-01-28 17:33:12 +0900

    Spring Core Basic - 스프링 컨테이너와 스프링 빈(스프링 빈 메타 정보 - BeanDefinition)

---
+++ Spring/Lecture/Spring-Core-Basic/src/test/java/my/study/springcorebasic/beandefinition/BeanDefinitionTest.java
@@ -0,0 +1,24 @@
+package my.study.springcorebasic.beandefinition;
+
+import my.study.springcorebasic.AppConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class BeanDefinitionTest {
+  AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+  // GenericXmlApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+
+  @Test
+  @DisplayName("빈 설정 메타정보 확인")
+  void findApplicationBean() {
+    String[] beanDefinitionNames = context.getBeanDefinitionNames();
+    for (String beanDefinitionName : beanDefinitionNames) {
+      BeanDefinition beanDefinition = context.getBeanDefinition(beanDefinitionName);
+      if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
+        System.out.println("beanDefinitionName" + beanDefinitionName + " beanDefinition = " + beanDefinition);
+      }
+    }
+  }
+}
