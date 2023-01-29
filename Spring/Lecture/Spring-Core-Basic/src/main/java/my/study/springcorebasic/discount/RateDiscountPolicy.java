2312e9a57ac9ae515f8ac7b644a71ca7e99ff701
Seohwan Choi <shchoice7140@gmail.com>
2023-01-29 18:34:49 +0900

    Spring Core Basic - 의존관계 주입(빈이 여러개일 때, @Primary)

--- Spring/Lecture/Spring-Core-Basic/src/main/java/my/study/springcorebasic/discount/RateDiscountPolicy.java
+++ Spring/Lecture/Spring-Core-Basic/src/main/java/my/study/springcorebasic/discount/RateDiscountPolicy.java
@@ -3,10 +3,12 @@ package my.study.springcorebasic.discount;
 import my.study.springcorebasic.member.Grade;
 import my.study.springcorebasic.member.Member;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
 @Qualifier("mainDiscountPolicy")
+@Primary
 public class RateDiscountPolicy implements DiscountPolicy {
   private int discountPercent = 10; //10% 할인
 
--- Spring/Lecture/Spring-Core-Basic/src/main/java/my/study/springcorebasic/order/OrderServiceImpl.java
+++ Spring/Lecture/Spring-Core-Basic/src/main/java/my/study/springcorebasic/order/OrderServiceImpl.java
@@ -1,21 +1,17 @@
 package my.study.springcorebasic.order;
 
+import lombok.RequiredArgsConstructor;
 import my.study.springcorebasic.discount.DiscountPolicy;
 import my.study.springcorebasic.member.Member;
 import my.study.springcorebasic.member.MemberRepository;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements  OrderService {
 
   private final MemberRepository memberRepository;
   private final DiscountPolicy discountPolicy;
-
-  public OrderServiceImpl(MemberRepository memberRepository, @Qualifier("mainDiscountPolicy") DiscountPolicy discountPolicy) {
-    this.memberRepository = memberRepository;
-    this.discountPolicy = discountPolicy;
-  }
 
   @Override
   public Order createOrder(Long memberId, String itemName, int itemPrice) {
