eb2dcb699d0df1b6a3549bd8a081f324725ece03
Seohwan Choi <shchoice7140@gmail.com>
2023-01-31 00:23:22 +0900

    Spring Core Basic - 빈 스코프(웹 스코프와 Provider)

---
+++ Spring/Lecture/Spring-Core-Basic/src/main/java/my/study/springcorebasic/web/LogDemoService.java
@@ -0,0 +1,16 @@
+package my.study.springcorebasic.web;
+
+import lombok.RequiredArgsConstructor;
+import my.study.springcorebasic.common.MyLogger;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LogDemoService {
+  private final ObjectProvider<MyLogger> myLoggerProvider;
+  public void logic(String id) {
+    MyLogger myLogger = myLoggerProvider.getObject();
+    myLogger.log("service id = " + id);
+  }
+}
