24961c7fe89de5ecd70c71a49bbaf0a02a09b877
Seohwan Choi <shchoice7140@gmail.com>
2023-01-30 23:32:01 +0900

    Spring Core Basic - 빈 스코프(싱글톤타입에서 프로토타입 사용 시 문제를 Provider를 통한 DL로 해결, 권장)

--- Spring/Lecture/Spring-Core-Basic/src/test/java/my/study/springcorebasic/scope/SingletonWithProtoTypeTest1.java
+++ Spring/Lecture/Spring-Core-Basic/src/test/java/my/study/springcorebasic/scope/SingletonWithProtoTypeTest1.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
@@ -33,19 +34,16 @@ public class SingletonWithProtoTypeTest1 {
 
     ClientBean clientBean2 = context.getBean(ClientBean.class);
     int count2 = clientBean2.logic();
-    assertThat(count2).isEqualTo(2);
+    assertThat(count2).isEqualTo(1);
   }
 
   @Scope("singleton")
   static class ClientBean {
-    private final PrototypeBean prototypeBean;
-
     @Autowired
-    public ClientBean(PrototypeBean prototypeBean) {  // 생성 시점에 주입
-      this.prototypeBean = prototypeBean;
-    }
+    private ObjectProvider<PrototypeBean> prototypeBeanProvider;
 
     public int logic() {
+      PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
       prototypeBean.addCount();
       int count = prototypeBean.getCount();
       return count;
