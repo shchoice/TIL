60cd3bd85bb304bfa259275d0e0ef2b413d1cdcf
Seohwan Choi <shchoice7140@gmail.com>
2023-02-02 01:51:24 +0900

    Spring-JPA-ExApp01 - 프로젝트 생성

---
+++ Spring/Spring-JPA-ExApp01/src/main/java/my/study/springjpaexapp01/SpringJpaExApp01Application.java
@@ -0,0 +1,13 @@
+package my.study.springjpaexapp01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaExApp01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJpaExApp01Application.class, args);
+    }
+
+}
---
+++ Spring/Spring-JPA-ExApp01/src/test/java/my/study/springjpaexapp01/SpringJpaExApp01ApplicationTests.java
@@ -0,0 +1,13 @@
+package my.study.springjpaexapp01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaExApp01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
