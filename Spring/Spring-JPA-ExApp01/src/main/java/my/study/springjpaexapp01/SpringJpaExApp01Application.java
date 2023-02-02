fbb9d0608557794dca813cfee63b074e30c93ff7
Seohwan Choi <shchoice7140@gmail.com>
2023-02-03 01:22:35 +0900

    Spring-JPA-ExApp01 - Thymeleaf View Test

--- Spring/Spring-JPA-ExApp01/src/main/java/my/study/springjpaexapp01/SpringJpaExApp01Application.java
+++ Spring/Spring-JPA-ExApp01/src/main/java/my/study/springjpaexapp01/SpringJpaExApp01Application.java
@@ -1,5 +1,6 @@
 package my.study.springjpaexapp01;
 
+import lombok.Getter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
