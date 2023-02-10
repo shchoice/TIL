dd0bc7ae0a61b08e37db8e3570cee63e7798d774
Seohwan Choi <shchoice7140@gmail.com>
2023-02-11 03:08:49 +0900

    Java ORM JPA Programming Basic - 영속성 컨텍스트의 이점(1), 1차 캐시

--- Spring/jpa-basic/src/main/java/my/stduy/jpabasic/hellojpa/JpaMain.java
+++ Spring/jpa-basic/src/main/java/my/stduy/jpabasic/hellojpa/JpaMain.java
@@ -23,6 +23,35 @@ public class JpaMain {
       em.persist(member01);
       em.persist(member02);
 
+      Member findMember = em.find(Member.class, 10L);
+
+      System.out.println("findMember.id = " + findMember.getId());
+      System.out.println("findMember.name = " + findMember.getName());
+      /*
+        findMember.id = 10
+        findMember.name = shchoi
+
+        insert my.stduy.jpabasic.hellojpa.Member
+        insert
+          into
+        Member
+            (name, id)
+        values
+            (?, ?)
+
+      Hibernate:
+        insert my.stduy.jpabasic.hellojpa.Member
+        insert
+          into
+        Member
+          (name, id)
+        values
+          (?, ?)
+
+        // select Query 가 나오지 않음을 알 수 있다.(1차 캐시에 있는 것을 조회했기 때문, DB에서 x)
+      */
+
+      // DB에 쿼리가 날아가 저장되는 시점
       tx.commit();
     } catch (Exception e) {
       tx.rollback();
