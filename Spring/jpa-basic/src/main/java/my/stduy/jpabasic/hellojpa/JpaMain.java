9500dd56291eb517a5c9f55696936751d3817b13
Seohwan Choi <shchoice7140@gmail.com>
2023-02-11 12:48:23 +0900

    Java ORM JPA Programming Basic - 영속성 컨텍스트의 이점(2), 영속 엔티티의 동일성 보장

--- Spring/jpa-basic/src/main/java/my/stduy/jpabasic/hellojpa/JpaMain.java
+++ Spring/jpa-basic/src/main/java/my/stduy/jpabasic/hellojpa/JpaMain.java
@@ -18,18 +18,7 @@ public class JpaMain {
       Member findMember01 = em.find(Member.class, 10L);
       Member findMember02 = em.find(Member.class, 10L);
 
-      /*
-        select
-          member0_.id as id1_0_0_,
-          member0_.name as name2_0_0_
-        from
-          Member member0_
-        where
-          member0_.id=?
-
-        해설 : findMember01을 통해 DB에서 조회를 하고 1차 캐시에 저장을 하기에
-              findMember02는 SQL을 통해 DB에 조회를 하지 않음을 확인할 수 있다.
-      */
+      System.out.println("result = " + (findMember01 == findMember02)); // true
 
       // DB에 쿼리가 날아가 저장되는 시점
       tx.commit();
