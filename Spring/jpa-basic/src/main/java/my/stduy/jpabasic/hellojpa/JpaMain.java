0ce60f1266f6d2fda36e8cf937a5c61823957fea
Seohwan Choi <shchoice7140@gmail.com>
2023-02-15 01:47:58 +0900

    Java ORM JPA Programming Basic - 필드 키 매핑, SEQUENCE(보충)

--- Spring/jpa-basic/src/main/java/my/stduy/jpabasic/hellojpa/JpaMain.java
+++ Spring/jpa-basic/src/main/java/my/stduy/jpabasic/hellojpa/JpaMain.java
@@ -18,23 +18,29 @@ public class JpaMain {
       Member member = new Member();
       member.setUsername("shchoice");
 
-      // IDENTITY 전략을 사용할 경우에는 commit 이전에 persist에서 insert를 수행
-      // 그렇지 않으면 ID를 매핑 시킬수 없기 때문
-      System.out.println("===============");
+      System.out.println("==========");
       em.persist(member);
-      System.out.println("===============");
+      // DB에 가서 SEQUENCE 정보를 받아오고 tx.commit에서 insert, DB 2번 다녀올 필요가 있나
+      // -> 대안 allocationSize 을 크게 해서 DB와 통신을 줄임 (allocation = 50 하면, 50개를 미리 받고 다른 데에서는 51번부터 사용)
+      System.out.println("member.id = " + member.getId());
+      System.out.println("==========");
       /*
-      ===============
-        insert
-            into
-        Member
-            (id, age, createDate, description, lastModifiedDate, roleType, name)
-        values
-            (default, ?, ?, ?, ?, ?, ?)
-      ===============
+       ==========
+       Hibernate:
+          call next value for MEMBER_SEQ
+       member.id = 1
+       ==========
        */
 
       tx.commit();
+      /*
+        insert
+        into
+            Member
+            (age, createDate, description, lastModifiedDate, roleType, name, id)
+        values
+            (?, ?, ?, ?, ?, ?, ?)
+       */
     } catch (Exception e) {
       tx.rollback();
     } finally {
