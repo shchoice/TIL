400c39ee95eca8895cf065d1c4104db6c33e029f
Seohwan Choi <shchoice7140@gmail.com>
2023-02-19 01:22:31 +0900

    Java ORM JPA Programming Basic - 연관관계 매핑(일대일 양방향)

--- Spring/jpa-basic/src/main/java/my/study/jpabasic/mapping/Locker.java
+++ Spring/jpa-basic/src/main/java/my/study/jpabasic/mapping/Locker.java
@@ -3,6 +3,7 @@ package my.study.jpabasic.mapping;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,4 +13,6 @@ public class Locker {
   @Id @GeneratedValue
   private Long id;
   private String name;
+  @OneToOne(mappedBy = "locker")
+  private Member member;
 }
