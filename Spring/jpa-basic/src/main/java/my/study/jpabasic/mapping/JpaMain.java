1873035122dd0129879f7143f0783ff6f03809cc
Seohwan Choi <shchoice7140@gmail.com>
2023-02-19 01:21:08 +0900

    Java ORM JPA Programming Basic - 연관관계 매핑(일대일 단방향)

--- Spring/jpa-basic/src/main/java/my/study/jpabasic/mapping/JpaMain.java
+++ Spring/jpa-basic/src/main/java/my/study/jpabasic/mapping/JpaMain.java
@@ -24,15 +24,6 @@ public class JpaMain {
       team.setName("teamA");
       team.getMembers().add(member);
       em.persist(team);
-      // OneToMany 의 문제점
-      // 1. team 을 수정했는데 member에 update Query가 나감
-      //    /* create one-to-many row my.study.jpabasic.mapping.Team.members */ update
-      //        member
-      //    set
-      //        team_id=?
-      //    where
-      //        id=?
-      // 2. OnetoMany를 잘 이해했다고 해도 실무에서 쿼리가 엄청 많아서 헷갈림..
 
       tx.commit();
     } catch (Exception e) {
--- Spring/jpa-basic/src/main/java/my/study/jpabasic/mapping/Member.java
+++ Spring/jpa-basic/src/main/java/my/study/jpabasic/mapping/Member.java
@@ -6,6 +6,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -14,13 +15,15 @@ import lombok.Setter;
 public class Member {
 
   @Id @GeneratedValue
+  @Column(name = "member_id")
   private Long id;
   @Column(name = "username")
   private String name;
   @ManyToOne
   @JoinColumn(name = "team_id", insertable = false, updatable = false)
   private Team team;
-
-
+  @OneToOne
+  @JoinColumn(name = "locker_id")
+  private Locker locker;
 }
 
