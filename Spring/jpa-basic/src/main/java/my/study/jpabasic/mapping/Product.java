8117e6207337f6f513e038d2296a1e1485949639
Seohwan Choi <shchoice7140@gmail.com>
2023-02-19 23:48:05 +0900

    Java ORM JPA Programming Basic - 연관관계 매핑(다대다 양방향, 권장X)

--- Spring/jpa-basic/src/main/java/my/study/jpabasic/mapping/Product.java
+++ Spring/jpa-basic/src/main/java/my/study/jpabasic/mapping/Product.java
@@ -1,8 +1,11 @@
 package my.study.jpabasic.mapping;
 
+import java.util.ArrayList;
+import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,4 +15,7 @@ public class Product {
   @Id @GeneratedValue
   private Long id;
   private String name;
+  @ManyToMany(mappedBy = "products")
+  private List<Member> members = new ArrayList<>();
+
 }
