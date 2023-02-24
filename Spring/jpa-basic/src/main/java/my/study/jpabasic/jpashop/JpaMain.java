83cf4f3bf72cbd27f57f8a9c60b6e27151d23996
Seohwan Choi <shchoice7140@gmail.com>
2023-02-25 00:34:34 +0900

    Java ORM JPA Programming Basic - 프록시와 연관관계 정리(준영속 시 에러 발생)

--- Spring/jpa-basic/src/main/java/my/study/jpabasic/jpashop/JpaMain.java
+++ Spring/jpa-basic/src/main/java/my/study/jpabasic/jpashop/JpaMain.java
@@ -23,42 +23,24 @@ public class JpaMain {
       em.flush();
       em.clear();
 
-      Member m1 = em.find(Member.class, member1.getId());
-      System.out.println("m1 : " + m1.getClass()); // m1 : class my.study.jpabasic.jpashop.domain.Member
+      Member refMember = em.getReference(Member.class, member1.getId());
+      System.out.println("refMember : " + refMember.getClass());
 
-      Member reference1 = em.getReference(Member.class, member1.getId());
-      System.out.println("reference1 : " + reference1.getClass()); // reference1 : class my.study.jpabasic.jpashop.domain.Member
+      em.detach(refMember);
 
-      System.out.println("m1 == reference : " + (m1 == reference1));
-      // true jpa는 한 영속성 컨텍스트에서 가져오고 동일한 pk를 사용하면 같게 해야하기 때문에,
-      // reference는 프록시가 아닌 실제 엔티티를 반환, 또한 1차 캐시를 사용하는 것이 프록시를 사용하는 것보다 낫기 때문
-
-      em.clear();
-
-
-      Member m2 = em.getReference(Member.class, member1.getId());
-      System.out.println("m2 : " + m2.getClass()); // m2 : class my.study.jpabasic.jpashop.domain.Member$HibernateProxy$E3jINkwO
-
-      Member reference2 = em.find(Member.class, member1.getId());
-      System.out.println("reference2 : " + reference2.getClass()); // reference2 : class my.study.jpabasic.jpashop.domain.Member$HibernateProxy$E3jINkwO
-
-      System.out.println("m2 == reference2 : " + (m2 == reference2)); // true
-
-      em.clear();
-
-
-      Member reference3 = em.getReference(Member.class, member1.getId());
-      System.out.println("reference3 : " + reference3.getClass()); // reference3 : class my.study.jpabasic.jpashop.domain.Member$HibernateProxy$E3jINkwO
-
-      Member reference4 = em.find(Member.class, member1.getId());
-      System.out.println("reference4 : " + reference4.getClass()); // reference4 : class my.study.jpabasic.jpashop.domain.Member$HibernateProxy$E3jINkwO
-
-      System.out.println("reference3 == reference4 : " + (reference3 == reference4)); // true
-
+      System.out.println("refMember : " + refMember.getName());
+      // org.hibernate.LazyInitializationException: could not initialize proxy [my.study.jpabasic.jpashop.domain.Member#1] - no Session
+      //	at org.hibernate.proxy.AbstractLazyInitializer.initialize(AbstractLazyInitializer.java:176)
+      //	at org.hibernate.proxy.AbstractLazyInitializer.getImplementation(AbstractLazyInitializer.java:322)
+      //	at org.hibernate.proxy.pojo.bytebuddy.ByteBuddyInterceptor.intercept(ByteBuddyInterceptor.java:45)
+      //	at org.hibernate.proxy.ProxyConfiguration$InterceptorDispatcher.intercept(ProxyConfiguration.java:95)
+      //	at my.study.jpabasic.jpashop.domain.Member$HibernateProxy$bsKnScm6.getName(Unknown Source)
+      //	at my.study.jpabasic.jpashop.JpaMain.main(JpaMain.java:31)
 
       tx.commit();
     } catch (Exception e) {
       tx.rollback();
+      e.printStackTrace();
     } finally {
       em.close();
     }
