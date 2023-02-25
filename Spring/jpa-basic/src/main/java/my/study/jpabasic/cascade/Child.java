6325677e923035686d5c1da00348e39e71f9e993
Seohwan Choi <shchoice7140@gmail.com>
2023-02-25 22:18:25 +0900

    Java ORM JPA Programming Basic - 프록시와 연관관계 정리(영속성 전이, CASCADE)

---
+++ Spring/jpa-basic/src/main/java/my/study/jpabasic/cascade/Child.java
@@ -0,0 +1,22 @@
+package my.study.jpabasic.cascade;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+public class Child {
+
+  @Id @GeneratedValue
+  private Long id;
+  private String name;
+
+  @ManyToOne
+  @JoinColumn(name = "parent_id")
+  private Parent parent;
+}
---
+++ Spring/jpa-basic/src/main/java/my/study/jpabasic/cascade/JpaMain.java
@@ -0,0 +1,36 @@
+package my.study.jpabasic.cascade;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+
+  public static void main(String[] args) {
+    EntityManagerFactory emf = Persistence.createEntityManagerFactory("cascade");
+    EntityManager em = emf.createEntityManager();
+    EntityTransaction tx = em.getTransaction();
+
+    tx.begin();
+
+    try {
+      Child child1 = new Child();
+      Child child2 = new Child();
+
+      Parent parent = new Parent();
+      parent.addChild(child1);
+      parent.addChild(child2);
+
+      em.persist(parent);
+
+      tx.commit();
+    } catch (Exception e) {
+      tx.rollback();
+      e.printStackTrace();
+    } finally {
+      em.close();
+    }
+    emf.close();
+  }
+}
