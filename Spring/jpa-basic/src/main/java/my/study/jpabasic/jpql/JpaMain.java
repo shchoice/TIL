d8c13a2535d506c734f924f4e4fb28e73cc461a0
Seohwan Choi <shchoice7140@gmail.com>
2023-02-27 23:56:14 +0900

    Java ORM JPA Programming Basic - 객체지향 쿼리 언어2(Join과 Fetch Join의 차이)

--- Spring/jpa-basic/src/main/java/my/study/jpabasic/jpql/JpaMain.java
+++ Spring/jpa-basic/src/main/java/my/study/jpabasic/jpql/JpaMain.java
@@ -57,12 +57,43 @@ public class JpaMain {
         for (Member_JPQL member_jpql: team.getMembers()) {
           System.out.println("-> Member = " + member_jpql);
         }
-//        result.size : 2
-//        member = 기반기술1팀, 2
-//        -> Member = Member_JPQL(id=3, username=shchoi, age=32, team=my.study.jpabasic.jpql.domain.Team@781711b7, orders=[])
-//        -> Member = Member_JPQL(id=4, username=shchoi2, age=33, team=my.study.jpabasic.jpql.domain.Team@781711b7, orders=[])
-//        member = 기반기술2팀, 1
-//        -> Member = Member_JPQL(id=5, username=shchoi3, age=33, team=my.study.jpabasic.jpql.domain.Team@65bb6275, orders=[])
+        // JOIN 과 FETCH JOIN 차이
+        // JOIN // • 일반 조인 실행시 연관된 엔티티를 함께 조회하지 않음
+        //    /* SELECT
+        //        DISTINCT t
+        //    FROM
+        //        Team t
+        //    JOIN
+        //        t.members */ select
+        //            distinct team0_.team_id as team_id1_3_,
+        //            team0_.name as name2_3_
+        //        from
+        //            team team0_
+        //        inner join
+        //            member members1_
+        //                on team0_.team_id=members1_.team_id
+
+        // FETCH JOIN
+        //     /* SELECT
+        //        DISTINCT t
+        //    FROM
+        //        Team t
+        //    JOIN
+        //        FETCH t.members */ select
+        //            distinct team0_.team_id as team_id1_3_0_,
+        //            members1_.member_id as member_i1_0_1_,
+        //            team0_.name as name2_3_0_,
+        //            members1_.age as age2_0_1_,
+        //            members1_.team_id as team_id4_0_1_,
+        //            members1_.username as username3_0_1_,
+        //            members1_.team_id as team_id4_0_0__,
+        //            members1_.member_id as member_i1_0_0__
+        //        from
+        //            team team0_
+        //        inner join
+        //            member members1_
+        //                on team0_.team_id=members1_.team_id
+
       }
 
       tx.commit();
