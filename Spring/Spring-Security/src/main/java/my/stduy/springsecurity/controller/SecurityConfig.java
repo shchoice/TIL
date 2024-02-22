361b78caa8beff885ad1871f068382fe8372292f
Seohwan Choi <shchoice7140@gmail.com>
2024-02-22 16:14:28 +0900

    feat(code): Form Login 인증

--- Spring/Spring-Security/src/main/java/my/stduy/springsecurity/controller/SecurityConfig.java
+++ Spring/Spring-Security/src/main/java/my/stduy/springsecurity/controller/SecurityConfig.java
@@ -1,9 +1,17 @@
 package my.stduy.springsecurity.controller;
 
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 @Configuration
 @EnableWebSecurity
@@ -15,6 +23,29 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         .authorizeRequests() // 요청에 대한 보안검사가 시작
         .anyRequest().authenticated(); //어떤 요청에도 인증을 받도록 함
     http
-        .formLogin(); // form 로그인 방식으로 인증을 할 수 있도록 함
+        .formLogin() // form 로그인 방식으로 인증을 할 수 있도록 함
+//        .loginPage("/loginPage")
+        .defaultSuccessUrl("/")
+        .failureUrl("/login")
+        .usernameParameter("username")
+        .passwordParameter("password")
+        .loginProcessingUrl("/login_proc")
+        .successHandler(new AuthenticationSuccessHandler() {
+          @Override
+          public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
+            System.out.println("authentication :" + authentication.getName());
+            httpServletResponse.sendRedirect("/");
+          }
+        })
+        .failureHandler(new AuthenticationFailureHandler() {
+          @Override
+          public void onAuthenticationFailure(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e) throws IOException, ServletException {
+            System.out.println("exception : " + e.getMessage());
+            httpServletResponse.sendRedirect("/login");
+          }
+        })
+        .permitAll() // loginPage 페이로 접근하는 모든 사용자들은 인가를 받지 않아도 접근이 가능하도록 함
+    ;
+
   }
 }
