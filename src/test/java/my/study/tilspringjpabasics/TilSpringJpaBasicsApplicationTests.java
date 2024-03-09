91ccde12bf288fa2f4e1323af4c5e49dc49263d4
Seohwan Choi <shchoice7140@gmail.com>
2024-03-09 22:54:53 +0900

    프로젝트 생성

---
+++ src/test/java/my/study/tilspringjpabasics/TilSpringJpaBasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package my.study.tilspringjpabasics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TilSpringJpaBasicsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
