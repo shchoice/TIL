9116efed67864a7105f4749e56250634ee31c3e4
Seohwan Choi <shchoice7140@gmail.com>
2024-03-10 19:47:05 +0900

    feat[code]: Hello JPA - JPQL 맛보기

--- src/main/java/my/study/tilspringjpabasics/JpaMain.java
+++ src/main/java/my/study/tilspringjpabasics/JpaMain.java
@@ -4,6 +4,7 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
+import java.util.List;
 import my.study.tilspringjpabasics.query.entity.Member;
 
 public class JpaMain {
@@ -15,32 +16,56 @@ public class JpaMain {
     tx.begin();
 
     try {
-      // 회원 객체 수정 - 수정할 때는 em.persist 를 사용하지 않아도 됨, Java Collection 하듯이 JPA가 만들어짐
-      Member findMember = em.find(Member.class, 1L);
-      findMember.setName("shchoice");
+      // JPA 입장에서는 코드를 짤 때 테이블을 대상으로 코드를 짜지 않고 멤버 객체를 대상으로 함
+      List<Member> members = em.createQuery("select m from Member as m", Member.class)
+          .getResultList();
+      for (Member member: members) {
+        System.out.println("member.getName() = " + member.getName());
+      }
       /*
-      Hibernate:
-        select
-            m1_0.id,
-            m1_0.name
+       Hibernate:
+        / * select
+            m
         from
+            Member as m * / select
+            m1_0.id,
+                m1_0.name
+            from
             Member m1_0
-        where
-            m1_0.id=?
-      Hibernate:
-      / * update
-          for my.study.tilspringjpabasics.query.entity.Member * /update Member
-      set
-        name=?
-      where
-        id=?
+      member.getName() = shchoice
+      member.getName() = jpa
       */
+
+      List<Member> members2 = em.createQuery("select m from Member as m", Member.class)
+          .setFirstResult(0)
+          .setMaxResults(10)
+          .getResultList();
+      for (Member member: members2) {
+        System.out.println("member.getName() = " + member.getName());
+      }
+      /*
+       Hibernate:
+        /* select
+            m
+          from
+              Member as m * / select
+                m1_0.id,
+                m1_0.name
+            from
+                Member m1_0
+            offset
+                ? rows
+            fetch
+                first ? rows only
+        member.getName() = shchoice
+        member.getName() = jpa
+        */
+      
       tx.commit();
     } catch (Exception e) {
       tx.rollback();
     } finally {
       em.close();
-
     }
     emf.close();
   }
