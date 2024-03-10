ea78862c1652cae97682d93e28f1afdaa05089f6
Seohwan Choi <shchoice7140@gmail.com>
2024-03-10 23:19:57 +0900

    feat[code]: 영속성 관리 - 영속성 컨텍스트의 이점

--- src/main/java/my/study/tilspringjpabasics/JpaMain.java
+++ src/main/java/my/study/tilspringjpabasics/JpaMain.java
@@ -16,30 +16,35 @@ public class JpaMain {
     tx.begin();
 
     try {
-      // 비영속
-      Member member = new Member();
-      member.setId(4L);
-      member.setName("persistence-detach");
+      // 1. 1차 캐시 비교 : findMember01 은 DB에서 읽어오지만 02는 1차 캐시로 읽어옴, 03은 DB에서 읽어옴
+      Member findMember01 = em.find(Member.class, 1L);
+      Member findMember02 = em.find(Member.class, 1L);
+      Member findMember03 = em.find(Member.class, 2L);
 
-      // 영속
-      System.out.println("--- before ---");
-      em.persist(member);
-      em.detach(member);
-      System.out.println("--- after ---");
+      // 2. 동일성 비교
+      System.out.println("isSame? = " + (findMember01 == findMember02)); // isSame? = true
+      System.out.println("isSame? = " + (findMember01 == findMember03)); // isSame? = false
 
-      // 준영속 상태로 영속 상태 처럼 로그는 아래와 같이 찍히지만 실제로는 DB에 반영이 안됨
-      /*
-      --- before ---
-      --- after ---
-      Hibernate:
-          / * insert for
-              my.study.tilspringjpabasics.query.entity.Member * /insert
-                into
-                  Member (name, id)
-                values
-                  (?, ?)
-       */
-      tx.commit();
+      // 3. 쓰기 지연
+      Member member01 = new Member(4L, "shchoice04");
+      Member member02 = new Member(5L, "shchoice05");
+
+      em.persist(member01);
+      em.persist(member02);
+      System.out.println("====================");
+
+      tx.commit();// 이 시점 이후로 persist가 동작함 <- 쓰기 지연으로 인해 버퍼링 기능을 사용할 수 있음
+      // persist 때마다 DB에 반영이 된다면 최적화할 수 있는 여지가 없게됨
+      // 한꺼번에 보내는 것을 JDBC Batch라 함
+      // hibernate에는 batch_size 옵션이 있음, batch_size 만큼 모으고 한번에 넣을 수 있음
+
+//      // 4. 변경 감지 (Dirty Caching엔티티 수정)
+//      Member findMember04 = em.find(Member.class, 4L);
+//      findMember04.setName("shchoice04-update");
+//      // em.update(member) 이러한 부류의 명령어 없이도 바로 쿼리가 날아감
+//      System.out.println("===============");
+//      tx.commit();
+
     } catch (Exception e) {
       tx.rollback();
     } finally {
--- src/main/java/my/study/tilspringjpabasics/query/entity/Member.java
+++ src/main/java/my/study/tilspringjpabasics/query/entity/Member.java
@@ -2,12 +2,16 @@ package my.study.tilspringjpabasics.query.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@RequiredArgsConstructor
+@AllArgsConstructor
 public class Member {
   @Id
   private Long id;
