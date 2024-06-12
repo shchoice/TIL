f58f7d2921ccd5c385c776a91ac672285293571f
Seohwan Choi <shchoice7140@gmail.com>
2024-06-12 09:54:42 +0900

    feat(Code): 테스트 코드 학습을 위한 프로젝트 생성

---
+++ Lectures/TDD/[FastCampus] Spring 환경에 바로 적용하는 테스트의 모든 것 초격차 패키지/02 간단 프로젝트로 아키텍처 및 단위테스트 실습하기/Board-Hexagonal-TDD/src/main/java/my/study/tdd/boardhexagonaltdd/BoardHexagonalTddApplication.java
@@ -0,0 +1,13 @@
+package my.study.tdd.boardhexagonaltdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardHexagonalTddApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BoardHexagonalTddApplication.class, args);
+  }
+
+}
---
+++ Lectures/TDD/[FastCampus] Spring 환경에 바로 적용하는 테스트의 모든 것 초격차 패키지/02 간단 프로젝트로 아키텍처 및 단위테스트 실습하기/Board-Hexagonal-TDD/src/test/java/my/study/tdd/boardhexagonaltdd/BoardHexagonalTddApplicationTests.java
@@ -0,0 +1,13 @@
+package my.study.tdd.boardhexagonaltdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardHexagonalTddApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
